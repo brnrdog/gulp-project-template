ec26264e84504538320634925adf8dec8be4b4ef
Bernardo Gurgel <bernardogfilho@gmail.com>
2014-02-03 01:33:50 -0300

    Add 'scripts' task to run jsHint and concatenate scripts close #2

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,17 @@
 
 'use strict';
 
-var gulp = require('gulp'),
-gutil    = require('gulp-util'),
+var gulp     = require('gulp'),
+gutil        = require('gulp-util'),
 sass         = require('gulp-ruby-sass'),
 autoprefixer = require('gulp-autoprefixer'),
-clean        = require('gulp-clean');
+clean        = require('gulp-clean'),
+jshint       = require('gulp-jshint'),
+concat       = require('gulp-concat');
 
-var stylesPath = 'app/assets/styles';
+// Paths variables
+var stylesPath = 'app/assets/styles',
+scriptsPath    = 'app/assets/scripts';
 
 
 gulp.task('styles', function () {
@@ -16,6 +20,14 @@ gulp.task('styles', function () {
   .pipe(sass({style: 'expanded'}), gutil.log('Compiling styles', 'Styles compiled'))
   .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
   .pipe(gulp.dest(stylesPath));
+});
+
+gulp.task('scripts', function () {
+  return gulp.src(scriptsPath + '/**/*.js')
+  .pipe(jshint(), gutil.log('Running jsHint'))
+  .pipe(jshint.reporter('default'))
+  .pipe(concat('script.js'), gutil.log('Concatenating scripts files', 'Done'))
+  .pipe(gulp.dest(scriptsPath));
 });
 
 gulp.task('clean', function () {
