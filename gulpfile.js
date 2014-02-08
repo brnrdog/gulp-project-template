57e1271099a8fdcb160a68a07251d0047533dea1
Bernardo Gurgel <bernardogfilho@gmail.com>
2014-02-08 00:36:54 -0300

    Make some refactorings

--- gulpfile.js
+++ gulpfile.js
@@ -9,55 +9,55 @@ sass         = require('gulp-ruby-sass'),
 autoprefixer = require('gulp-autoprefixer'),
 jshint       = require('gulp-jshint'),
 coffee       = require('gulp-coffee'),
-clean       = require('gulp-clean'),
+clean        = require('gulp-clean'),
 connect      = require('gulp-connect'),
 usemin       = require('gulp-usemin');
 
-// paths variables
-var stylesPath = 'app/assets/styles',
-scriptsPath    = 'app/assets/scripts';
-
-gulp.task('styles', function () {
-  return gulp.src(stylesPath + '/**/*.scss')
-  .pipe(sass({style: 'expanded'}), gutil.log('Compiling styles', 'Styles compiled'))
-  .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-  .pipe(gulp.dest(stylesPath));
-});
-
-gulp.task('coffee', function () {
-  return gulp.src(scriptsPath + '/**/*.coffee')
-  .pipe(coffee({bare: true})).on('error', gutil.log)
-  .pipe(gulp.dest(scriptsPath));
-});
-
-gulp.task('scripts', ['coffee'], function () {
-  return gulp.src(scriptsPath + '/**/*.js')
-  .pipe(jshint(), gutil.log('Running jsHint'))
-  .pipe(jshint.reporter('default'))
-  .pipe(gulp.dest(scriptsPath));
-});
-
+// Connect Task
 gulp.task('connect', connect.server({
   root: './app',
   port: 1337,
-  livereload: true,
-  open: {
-    file: 'index.html',
-    browser: 'Google Chrome'
-  }
+  livereload: true
 }));
 
-gulp.task('watch', function () {
-  gulp.watch([ stylesPath + '/**/*.scss'], ['styles']);
-  gulp.watch([ scriptsPath + '/**/*.coffee'], ['scripts']);
-  gulp.watch([
-    './app/**/*.html',
-    './app/assets/styles/**/*.css',
-    './app/assets/scripts/**/*.js'
-  ], connect.reload);
+gulp.task('html', ['coffee'], function () {
+  return gulp.src('./app/**/*.html')
+    .pipe(connect.reload());
 });
 
-gulp.task('serve', ['connect', 'styles', 'scripts', 'watch']);
+// Scss compiler task
+gulp.task('scss', function () {
+  gulp.src('./app/assets/styles**/*.scss')
+  .pipe(sass({style: 'expanded'}), gutil.log('Compiling scss files...'))
+  .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'), gutil.log('Applying autoprefixer...'))
+  .pipe(gulp.dest('./app/assets/styles/'))
+  .pipe(connect.reload(), gutil.log('Reloading browser...'));
+});
+
+// CoffeeScript compiler task
+gulp.task('coffee', function () {
+  return gulp.src('app/assets/scripts/**/*.coffee')
+    .pipe(coffee({bare: true})).on('error', gutil.log)
+    .pipe(gulp.dest('app/assets/scripts'));
+});
+
+// JSHint task
+gulp.task('scripts', ['coffee'], function () {
+  return gulp.src('app/assets/scripts/**/*.js')
+    .pipe(jshint(), gutil.log('Running jsHint'))
+    .pipe(jshint.reporter('default'))
+    .pipe(gulp.dest('app/assets/scripts'))
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch([ 'app/assets/styles/**/*.scss'], ['scss']);
+  gulp.watch([ 'app/assets/styles/**/*.css'], connect.reload);
+  gulp.watch([ 'app/assets/scripts' + '/**/*.coffee'], ['scripts']);
+  gulp.watch(['./app/**/*.html'], ['html']);
+});
+
+gulp.task('serve', ['connect', 'scss', 'scripts', 'watch']);
 
 gulp.task('clean', function () {
   gutil.log('Clean task goes here...');
@@ -65,16 +65,16 @@ gulp.task('clean', function () {
 
 gulp.task('usemin', function () {
   gulp.src('./app/**/*.html')
-  .pipe(usemin(), gutil.log('Running usemin...'))
-  .pipe(gulp.dest('dist/'));
+    .pipe(usemin(), gutil.log('Running usemin...'))
+    .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('clean-build', function () {
   gulp.src('dist/', {read: false})
-  .pipe(clean());
+    .pipe(clean());
 });
 
-gulp.task('build', ['styles', 'scripts', 'clean-build'], function () {
+gulp.task('build', ['scss', 'scripts', 'clean-build'], function () {
   return gulp.start('usemin');
 });
 
