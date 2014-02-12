9af212653d67f68b56a8f117e3bc9a45d28fe756
Bernardo Gurgel <bernardogfilho@gmail.com>
2014-02-12 15:53:23 -0300

    Add mocha to test gulp

---
+++ test/test.js
@@ -0,0 +1,11 @@
+/* globals require, describe, it */
+
+'use strict';
+
+var assert = require('assert');
+
+describe('dumbTest', function(){
+  it('return pass always', function(){
+    assert.equal(true, true);
+  });
+});
